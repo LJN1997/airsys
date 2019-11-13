9a70ae44cc35bbf25cc611c01d4f95ec0ed465d2
Jm <648183937@qq.com>
2019-11-13 08:46:59 +0800

    航班状态

--- src/main/java/dao/impl/AirsysDaoImpl.java
+++ src/main/java/dao/impl/AirsysDaoImpl.java
@@ -14,6 +14,7 @@ import entity.UserSelect;
 import entity.UserSelectAll;
 @Repository("accountDaoSpringImpl")
 public class AirsysDaoImpl implements IAirsysDao{
+	
 	@Autowired
 	private JdbcTemplate jdbctemplate;
 
